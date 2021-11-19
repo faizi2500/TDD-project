c4e29d2c9f61f87ee36276521decddeb2971f7f3
ali abbani <aliox@ahlam.com>
2021-11-19 12:42:43 +0200

    Fix linter errors

--- solver.rb
+++ solver.rb
@@ -1,27 +1,27 @@
 class Solver
-    def factorial(n)
-        if n == 0
-            1
-        elsif n > 0
-            n * factorial(n-1)
-        else 
-            'Oh, there is an error. Please use positive number'
-        end
+  def factorial(num)
+    if num.zero?
+      1
+    elsif num.positive?
+      num * factorial(num - 1)
+    else
+      'Oh, there is an error. Please use positive number'
     end
+  end
 
-    def reverse(str)
-        str.reverse
+  def reverse(str)
+    str.reverse
+  end
+
+  def fizzbuzz(num)
+    if num.modulo(3).zero? && num.modulo(5).zero?
+      'fizzbuzz'
+    elsif num.modulo(3).zero?
+      'fizz'
+    elsif num.modulo(5).zero?
+      'buzz'
+    else
+      num.to_s
     end
-    
-    def fizzbuzz(n)
-        if n.modulo(3) == 0 && n.modulo(5) == 0
-            'fizzbuzz'
-        elsif n.modulo(3) == 0
-            'fizz'
-        elsif n.modulo(5) == 0
-            'buzz'
-        else
-            n.to_s
-        end
-    end
+  end
 end
