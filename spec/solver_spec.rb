bcbf52a692ac27ba197ea9a8dc4d67ebbd9f7628
faizi2500 <faizan.zahid2500@gmail.com>
2021-11-19 15:13:59 +0500

    Add tests for fizzbuzz mthod

--- spec/solver_spec.rb
+++ spec/solver_spec.rb
@@ -4,7 +4,7 @@ describe Solver do
    before :each do
       @solver = Solver.new
    end
-
+    context '#factorial' do
         it "should test the factorial method" do 
             expect(@solver.factorial(4)).to eq 24
         end 
@@ -14,9 +14,33 @@ describe Solver do
         it "should raise an exception if a negative integer is given " do 
             expect(@solver.factorial(-4)).to eq 'Oh, there is an error. Please use positive number'
         end 
+    end
 
-
+    context '#reverse' do
         it "should return the string word reversed " do 
             expect(@solver.reverse("faizan")).to eq 'naziaf'
         end 
+
+        it "should return the string word reversed " do 
+            expect(@solver.reverse("faizan")).not_to eq 'faizan'
+        end 
+    end
+
+    context '#fizzbuzz' do
+        it "should return 'fizz' if divisible by 3 only" do 
+            expect(@solver.fizzbuzz(9)).to eq 'fizz'
+        end 
+
+        it "should return 'buzz' if divisible by 5 only " do 
+            expect(@solver.reverse()).not_to eq 'faizan'
+        end 
+
+        it "should return 'fizzbuzz' if divisible by 3 and 5 both " do 
+            expect(@solver.reverse("faizan")).not_to eq 'faizan'
+        end 
+
+        it "throw error if divisble by none " do 
+            expect(@solver.reverse(10)).not_to eq 'Not divisble'
+        end 
+    end
 end
